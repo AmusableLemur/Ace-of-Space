206456033b3016d9767b9a1148ddd3def1b39183
Rasmus Larsson <meister@rasmuslarsson.se>
2012-12-09 21:47:02 +0100

    Changed saucer to target center of player

--- src/entities/enemy/Saucer.java
+++ src/entities/enemy/Saucer.java
@@ -9,14 +9,14 @@ public class Saucer extends Enemy {
 
         setScore(100);
     }
-    
+
     @Override
     public void update(GameContainer gc, int delta, Player player) {
         super.update(gc, delta, player);
         graphic.rotate(2);
 
         // TODO: Change to be actual angle towards player
-        if (player.getX() > getX()) {
+        if (player.getCenterX() > getCenterX()) {
             setX(getX() + (float)speed * delta / 4);
         } else {
             setX(getX() - (float)speed * delta / 4);
